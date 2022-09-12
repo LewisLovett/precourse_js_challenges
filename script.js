88cbf30c27d0ab92999e13e75d5afa9fa299cbe1
Lewis Lovett <llovett7575@gmail.com>
2022-09-12 15:46:32 +0100

    Created getLargestNumber function

--- script.js
+++ script.js
@@ -9,9 +9,21 @@ const isNumberPositive = (num) => {
 console.log(isNumberPositive(-1));
 console.log(isNumberPositive(10));
 
-convertDaysToAge = (age) => {
+const convertDaysToAge = (age) => {
     return (Math.floor(age/365));
 }
 
 console.log(convertDaysToAge(3650));
 console.log(convertDaysToAge(6570));
+
+const getLargestNumber = (x,y,z) => {
+    if(x>y){
+        if(x>z){
+            return x;
+        }
+    }else if(y>z){
+        return y;
+    }else{
+        return z;
+    }
+}
